5fb8e372a0d7ec298b9c8fe2d61838045d4ef22e
Thomas John <john.th@northeastern.edu>
2022-11-30 17:49:15 -0500

    insert qureies

---
+++ Insert Queries/insertDelivery.sql
@@ -0,0 +1,14 @@
+USE WareHouseManagementSystem;
+
+INSERT INTO [Delivery] (deliveryId, orderId, addressId, [status])
+VALUES 
+(1, 1, 1, 1),
+(2, 2, 2, 0),
+(3, 3, 3, 1),
+(4, 4, 4, 1),
+(5, 5, 5, 0),
+(6, 6, 6, 1),
+(7, 7, 7, 0),
+(8, 8, 8, 0),
+(9, 9, 9, 1),
+(10, 10, 10, 1);
