33b2a5a720f6c48fbf0d23133d7d177a981d1c16
Santosh Naranapatty <naranapatty.s@northeastern.edu>
2022-12-01 02:41:17 -0500

    username constraint

--- Table Level Constriants/constraints.sql
+++ Table Level Constriants/constraints.sql
@@ -3,3 +3,7 @@ USE WareHouseManagementSystem;
 -- Constraint to validate email id
 ALTER TABLE [User]
 ADD CONSTRAINT EmailCheck  CHECK(email LIKE '%___@___%.__%')
+
+-- Constraint to prevent duplicate username
+ALTER TABLE [User] 
+ADD CONSTRAINT [IX_UniqueUserName] UNIQUE NONCLUSTERED ([username] ASC)
