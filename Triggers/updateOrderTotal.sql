ba444962b53e27991b979e4e4a42ae6607205c25
Thomas John <john.th@northeastern.edu>
2022-12-01 19:48:44 -0500

    stock update for purchase and customer order

--- Triggers/updateOrderTotal.sql
+++ Triggers/updateOrderTotal.sql
@@ -1,23 +1,30 @@
-USE WareHouseManagementSystem;
 
 CREATE TRIGGER updateOrderTotal
 ON dbo.[OrderItem]
 AFTER INSERT, UPDATE
 AS
 BEGIN
-	DECLARE @orderID INT;
-    DECLARE @stockItemId INT;
-    DECLARE @stockQty INT;
-    DECLARE @orderOty INT;
-    DECLARE @orderTotal DECIMAL;
+    IF UPDATE (quantity) 
+    BEGIN
+        DECLARE @orderID INT;
+        DECLARE @stockItemId INT;
+        DECLARE @stockQty INT;
+        DECLARE @orderOty INT;
+        DECLARE @orderTotal DECIMAL;
+        DECLARE @orderType INT;
 
-    Select 
-	@orderID = orderId, @stockItemId = orderItemId, @orderOty=quantity
-    FROM inserted;
-            SELECT @orderTotal = dbo.computeOrderTotalPrice(@orderID);
-            UPDATE [Order] SET total = @orderTotal WHERE orderId = @orderID;
-            SELECT @stockQty = dbo.checkStock(@stockItemId)-@orderOty;
-            UPDATE StockItem SET quantity=@stockQty,
-            availability = CASE WHEN @stockQty < 0 THEN 'n' ELSE 'y' END
-            WHERE itemId=@stockItemId;
+        SELECT 
+        @orderID = orderId, @stockItemId = orderItemId, @orderOty=quantity
+        FROM inserted;
+                SELECT @orderTotal = dbo.computeOrderTotalPrice(@orderID);
+                UPDATE [Order] SET total = @orderTotal WHERE orderId = @orderID;
+                SELECT @orderType = dbo.getOrderType(@orderID);
+                IF @orderType =1
+                    SELECT @stockQty = dbo.checkStock(@stockItemId)@orderOty;
+                ELSE
+                    SELECT @stockQty = dbo.checkStock(@stockItemId)+@orderOty;
+                UPDATE StockItem SET quantity=@stockQty,
+                availability = CASE WHEN @stockQty <= 0 THEN 'n' ELSE 'y' END
+                WHERE itemId=@stockItemId;
+    END
 END
